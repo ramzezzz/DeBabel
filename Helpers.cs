df20251974a449da647a166afb041c7109696500
Alex Perev <ramzezzz@mail.ru>
2019-11-20 11:40:20 +0300

    Добавьте файлы проекта.

---
+++ Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabelVMRestore
+{
+    public static class Helpers
+    {
+        public static object GetInstanceField(Type type, object instance, string fieldName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Static;
+            FieldInfo field = type.GetFields(bindFlags).GetValue(0) as FieldInfo;
+            var value = field.GetValue(instance);
+
+            var s = value.GetType().ToString();
+            if (!Guid.TryParse(s, out var _)) return value;
+            value = (DynamicMethod)(value.GetType().GetFields(bindFlags)[0].GetValue(value));
+
+            return value;
+        }
+    }
+}
